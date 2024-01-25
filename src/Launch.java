65aa64817d1d92a3a45a2d6e2ba21c887a42ca59
Erik Winbladh <erik.j.winbladh@gmail.com>
2024-01-25 11:53:56 +0100

    Issue1/code skeleton  (#40)
    * Add code skeleton for DECIDE(). 
    
    ---------
    
    Co-authored-by: Douglas Fischer <anakin_skryter@hotmail.se>
    Co-authored-by: Robin Claesson <robin.claesson@gmail.com>
    Co-authored-by: Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
    Co-authored-by: Erik Vinblad <erik.j.winbladh@gmail.com>

---
+++ src/Launch.java
@@ -0,0 +1,11 @@
+public class Launch {
+
+    public static boolean[][] calcPUM(LCMOperators[][] lcm, boolean[] cmv) {
+        return new boolean[15][15];
+    }
+
+    public static boolean[] calcFUV(boolean[][] pum, boolean[] puv) {
+        return new boolean[15];
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.geom.Point2D;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Parameters PARAMETERS = new Parameters(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		Point2D.Double[] POINTS = new Point2D.Double[0];
+		LCMOperators[][] LCM = new LCMOperators[15][15];
+		boolean[] PUV = new boolean[15];
+		DECIDE(POINTS.length, POINTS, PARAMETERS, LCM, PUV);
+	}
+
+	public static void DECIDE(int NUMPOINTS, Point2D.Double[] POINTS, Parameters PARAMETERS, LCMOperators[][] LCM, boolean[] PUV) {}
+
+}
