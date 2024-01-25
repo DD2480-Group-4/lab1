a97e31ca6e450e41fe017c7726a1119a8e0ba625
ractodev <erik.j.winbladh@gmail.com>
2024-01-25 10:59:16 +0100

    Add code skeleton for CMV and Parameters.

---
+++ src/CMV.java
@@ -0,0 +1,82 @@
+import java.awt.geom.Point2D;
+
+public class CMV {
+    private final int NUMPOINTS;
+    private final Point2D.Double[] POINTS;
+    private final Parameters PARAMETERS;
+
+    public CMV(int NUMPOINTS, Point2D.Double[] POINTS, Parameters PARAMETERS) {
+        this.NUMPOINTS = NUMPOINTS;
+        this.POINTS = POINTS;
+        this.PARAMETERS = PARAMETERS;
+    }
+
+    public boolean[] calcCMV() {
+        return new boolean[]{
+            LIC0(), LIC1(), LIC2(), LIC3(), LIC4(),
+            LIC5(), LIC6(), LIC7(), LIC8(), LIC9(),
+            LIC10(), LIC11(), LIC12(), LIC13(), LIC14()
+        };
+    }
+
+    public boolean LIC0() {
+        return false;
+    }
+
+    public boolean LIC1() {
+        return false;
+    }
+
+    public boolean LIC2() {
+        return false;
+    }
+
+    public boolean LIC3() {
+        return false;
+    }
+
+    public boolean LIC4() {
+        return false;
+    }
+
+    public boolean LIC5() {
+        return false;
+    }
+
+    public boolean LIC6() {
+        return false;
+    }
+
+    public boolean LIC7() {
+        return false;
+    }
+
+    public boolean LIC8() {
+        return false;
+    }
+
+    public boolean LIC9() {
+        return false;
+    }
+
+    public boolean LIC10() {
+        return false;
+    }
+
+    public boolean LIC11() {
+        return false;
+    }
+
+    public boolean LIC12() {
+        return false;
+    }
+
+    public boolean LIC13() {
+        return false;
+    }
+
+    public boolean LIC14() {
+        return false;
+    }
+
+}
---
+++ src/Parameters.java
@@ -0,0 +1,21 @@
+public record Parameters (
+    double LENGTH1,
+    double LENGTH2,
+    double RADIUS1,
+    double RADIUS2,
+    double EPSILON,
+    double AREA1,
+    double AREA2,
+    double DIST,
+    int Q_PTS,
+    int QUADS,
+    int N_PTS,
+    int K_PTS,
+    int A_PTS,
+    int B_PTS,
+    int C_PTS,
+    int D_PTS,
+    int E_PTS,
+    int F_PTS,
+    int G_PTS
+){}
