ed7ff33c8b5f3fcdebeb0bbf85073bc9b6c7a121
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2024-01-26 15:13:16 +0100

    feat: Added utility function to calculate area of triangle (#45)
    Closes: #44

--- src/test/java/decide/UtilitiesTests.java
+++ src/test/java/decide/UtilitiesTests.java
@@ -3,5 +3,33 @@ package decide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
+import java.awt.geom.Point2D;
+
 public class UtilitiesTests {
+
+    @Test
+    @DisplayName("calculateTriangleArea: Integer Vertices")
+    void calculateTriangleArea_IntegerVertices_CalculatesAreaCorrect()
+    {
+        Point2D.Double p1 = new Point2D.Double(1, 1);
+        Point2D.Double p2 = new Point2D.Double(-1, 1);
+        Point2D.Double p3 = new Point2D.Double(1, -1);
+
+        double area = Utilities.calculateTriangleArea(p1, p2, p3);
+
+        Assertions.assertThat(area).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("calculateTriangleArea: Decimal Vertices")
+    void calculateTriangleArea_DecimalVertices_CalculatesAreaCorrect()
+    {
+        Point2D.Double p1 = new Point2D.Double(1.5, 1.55);
+        Point2D.Double p2 = new Point2D.Double(-1.5, 1.51);
+        Point2D.Double p3 = new Point2D.Double(1.5, -1.3);
+
+        double area = Utilities.calculateTriangleArea(p1, p2, p3);
+
+        Assertions.assertThat(area).isEqualTo(4.275);
+    }
 }
