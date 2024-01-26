83a00fa8cc8f1f1f4af4a61490b322ecea1ba7ed
Robin Claesson <robcla@kth.se>
2024-01-26 14:52:47 +0100

    Added utility function to calculate area of triangle

--- src/main/java/decide/Utilities.java
+++ src/main/java/decide/Utilities.java
@@ -1,6 +1,11 @@
 package decide;
 
+import java.awt.geom.Point2D;
 
 public class Utilities {
-    
+ 
+    public static double calculateTriangleArea(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
+        return Math.abs((p1.x*(p2.y-p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y-p2.y))/2);
+    }
+
 }
