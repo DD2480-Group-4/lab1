7b78a0c58ff0bde48775b3aa329711b15c9f7fd5
Robin Claesson <robcla@kth.se>
2024-01-26 13:36:04 +0100

    Test suite for decide

---
+++ src/test/java/decide/CMVTests.java
@@ -0,0 +1,10 @@
+package decide;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class CMVTests {
+
+
+}
---
+++ src/test/java/decide/LaunchTests.java
@@ -0,0 +1,7 @@
+package decide;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+public class LaunchTests {
+}
