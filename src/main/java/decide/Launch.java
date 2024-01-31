0d20f626183896c81bcb0ec14279368927cd5fe4
Douglas Fischer <anakin_skryter@hotmail.se>
2024-01-31 15:13:33 +0100

    add functionality for calcPUM

--- src/main/java/decide/Launch.java
+++ src/main/java/decide/Launch.java
@@ -4,7 +4,20 @@ package decide;
 public class Launch {
 
     public static boolean[][] calcPUM(LCMOperators[][] lcm, boolean[] cmv) {
-        return new boolean[15][15];
+        boolean[][] pum = new boolean[15][15];
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (lcm[i][j] == LCMOperators.ANDD) {
+                    pum[i][j] = (cmv[i] && cmv[j]);
+                } else if (lcm[i][j] == LCMOperators.ORR) {
+                    pum[i][j] = (cmv[i] || cmv[j]);
+                } else {
+                    //NOTUSED
+                    pum[i][j] = true;
+                }
+            }
+        }
+        return pum;
     }
 
     public static boolean[] calcFUV(boolean[][] pum, boolean[] puv) {
