0c01d3cdf79091f13a148c71908f84211ee5e987
ractodev <erik.j.winbladh@gmail.com>
2024-01-31 14:46:54 +0100

    fix: change in tests to account for radians instead of degrees

--- src/test/java/decide/CMVTests.java
+++ src/test/java/decide/CMVTests.java
@@ -232,34 +232,40 @@ public class CMVTests {
     }    
 	
 	@Test
-    @DisplayName("LIC9: satisfied condition returns true")
-    void LIC9_threeSeparatedDataPointsFormSatisfiedAngle_ReturnsTrue() {
-        Parameters PARAMETERS = new Parameters(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0);
-		Point2D.Double[] POINTS = {
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(-10,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,10)};
-        CMV cmv = new CMV(POINTS.length, POINTS, PARAMETERS);
-        // 45-degree angle should be larger than (PI + 1)
+    @DisplayName("LIC9: satisfied general-case")
+    void LIC9_GeneralCase_ReturnsTrue() {
+        var points = new Point2D.Double[]{
+                new Point2D.Double(1, 0.001),
+                new Point2D.Double(0, 0),
+                new Point2D.Double(0, 1),
+                new Point2D.Double(0, 1),
+                new Point2D.Double(0, 1)
+        };
+        CMV cmv = new CMV(
+                points.length, points, new Parameters(
+                0, 0, 0, 0, Math.PI / 2, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        )
+        );
         Assertions.assertThat(cmv.LIC9()).isTrue();
     }
 
     @Test
-    @DisplayName("LIC9: unsatisfied condition returns false")
-    void LIC9_threeSeparatedDataPointsFormUnsatisfiedAngle_ReturnsFalse() {
-        Parameters PARAMETERS = new Parameters(0, 0, 0, 0, 45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0);
-		Point2D.Double[] POINTS = {
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(-10,0),
-                new Point2D.Double(0,0),
-                new Point2D.Double(0,10)};
-        CMV cmv = new CMV(POINTS.length, POINTS, PARAMETERS);
-        // 45-degree angle should not be larger than (PI + 45)
+    @DisplayName("LIC9: unsatisfied general-case")
+    void LIC9_GeneralCase_ReturnsFalse() {
+        var points = new Point2D.Double[]{
+                new Point2D.Double(1, 0),
+                new Point2D.Double(0, 0),
+                new Point2D.Double(-1, 3),
+                new Point2D.Double(-100, 1),
+                new Point2D.Double(-200, 1)
+        };
+        CMV cmv = new CMV(
+                points.length, points, new Parameters(
+                0, 0, 0, 0, Math.PI / 2, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        )
+        );
         Assertions.assertThat(cmv.LIC9()).isFalse();
     }
 }
